Metrics fix and loss scaling

project/model/metrics.py
@@ -25,7 +25,7 @@ class Loss(Metric):
         return f"LossSeqence"
 
     def __call__(self, output, sample):
-        return self._loss(output, sample)
+        return self._loss(torch.tensor(output), torch.tensor(sample))
 
 
 class WholeSeqence(Metric):
@@ -34,6 +34,8 @@ class WholeSeqence(Metric):
         return f"WholeSeqence"
 
     def __call__(self, output, sample):
+        sample = torch.tensor(sample)
+        output = torch.tensor(output)
         return (
             torch.abs(sample - output) / (torch.abs(sample) + torch.abs(output) + 1e-9)
         ).mean()

project/model/sequence.py
@@ -19,9 +19,9 @@ class SequencePredictionModel(pl.LightningModule):
             + [nn.LSTMCell(hidden_size, hidden_size) for _ in range(num_layers - 1)]
         ).to(self.device)
         self.fc = nn.Linear(hidden_size, output_size).to(self.device)
-        self._loss = nn.MSELoss()
+        self._loss = nn.MSELoss(reduction="none")
         self._metrics = [Seqence(-1), Seqence(0)]
-        self._iteration_metrics = [Loss(self._loss), WholeSeqence()]
+        self._iteration_metrics = [Loss(nn.MSELoss()), WholeSeqence()]
 
     def forward(self, x):
         output = []
@@ -42,14 +42,24 @@ class SequencePredictionModel(pl.LightningModule):
     def training_step(self, batch):
         x, y = batch
         y_pred, _ = self(x)
-        loss = self._loss(y_pred, y)
-        self._report("training", y_pred, y, self._iteration_metrics)
+        loss = (self._loss(y_pred, y) / (y + y_pred + 1e-9)).mean()
+        self._report(
+            "training",
+            y_pred.detach().cpu().squeeze(-1).numpy().tolist(),
+            y.cpu().squeeze(-1).numpy().tolist(),
+            self._iteration_metrics,
+        )
         return loss
 
     def validation_step(self, batch):
         x, y = batch
         y_pred, _ = self(x)
-        self._report("validation", y_pred, y, self._iteration_metrics)
+        self._report(
+            "validation",
+            y_pred.cpu().squeeze(-1).numpy().tolist(),
+            y.cpu().squeeze(-1).numpy().tolist(),
+            self._iteration_metrics,
+        )
 
     def test_step(self, batch):
         x, _ = batch